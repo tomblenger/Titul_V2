e8e3b85e7812a3a37ef6475cc1c65fb1571dc4d2
PassionIntelligence-1225 <Passion.Intell@outlook.com>
2025-03-25 06:53:09 -0700

    Phoenix 2025-03-25 22:45 refresh issue

--- v2/js/custom.js
+++ v2/js/custom.js
@@ -128,8 +128,9 @@ $(document).ready(function() {
 
 wow = new WOW({
     animateClass: 'animated',
-    offset: 100,
-    live: false
+    // offset: 100,
+    live: false,
+    resetAnimation: false
 });
 
 wow.init();
