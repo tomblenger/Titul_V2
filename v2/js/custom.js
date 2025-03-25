fc46708257430b5e3dc0cb25f4dc79b1081eaa8d
PassionIntelligence-1225 <Passion.Intell@outlook.com>
2025-03-25 07:42:19 -0700

    Phoenix 2025-03-25 23:35 refresh issue

--- v2/js/custom.js
+++ v2/js/custom.js
@@ -124,20 +124,21 @@ $(document).ready(function() {
     // alert("ready")
 
     // Apply smooth scrolling only to header links
-    $('a[href^="#"]').on('click', function (event) {
-        // Prevent default anchor click behavior
-        event.preventDefault();
 
-        // Get the target element
-        var target = $($.attr(this, 'href'));
-
-        // If the target exists, scroll to it smoothly
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1200); // Adjust the scroll speed (in ms)
-        }
-    });
+    // $('a[href^="#"]').on('click', function (event) {
+    //     // Prevent default anchor click behavior
+    //     event.preventDefault();
+    //
+    //     // Get the target element
+    //     var target = $($.attr(this, 'href'));
+    //
+    //     // If the target exists, scroll to it smoothly
+    //     if (target.length) {
+    //         $('html, body').animate({
+    //             scrollTop: target.offset().top
+    //         }, 600); // Adjust the scroll speed (in ms)
+    //     }
+    // });
 
 });
 
